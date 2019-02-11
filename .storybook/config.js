3e0b9592f5047d3286adf22a5a82dcad5ff0abc4
Gonçalo Morais <goncalo@bridge-u.com>
2019-02-11 17:54:03 +0000

    Move stories folder
    Just as the documentation reads:
    > There are no rules for this, but in general, stories are easier
    > to maintain when they are located closer to components.
    > https://storybook.js.org/basics/writing-stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,7 @@
 import { configure } from '@storybook/html';
 
 function loadStories() {
-  require('../stories/index.js');
+  require('../src/stories/index.js');
   // You can require as many stories as you need.
 }
 
---
+++ src/stories/index.js
@@ -0,0 +1,25 @@
+/* global document */
+import { storiesOf } from '@storybook/html';
+
+import '../app.js';
+
+storiesOf('Demo', module)
+  .add('heading', () => '<h1>Hello World</h1>')
+  .add('button', () => {
+    const button = document.createElement('button');
+    button.innerText = 'Hello Button';
+    button.className = 'btn btn-primary';
+    button.addEventListener('click', e => console.log(e));
+    return button;
+  })
+  .add('buttons', () => `
+    <button type="button" class="btn btn-primary">Primary</button>
+    <button type="button" class="btn btn-secondary">Secondary</button>
+    <button type="button" class="btn btn-success">Success</button>
+    <button type="button" class="btn btn-danger">Danger</button>
+    <button type="button" class="btn btn-warning">Warning</button>
+    <button type="button" class="btn btn-info">Info</button>
+    <button type="button" class="btn btn-light">Light</button>
+    <button type="button" class="btn btn-dark">Dark</button>
+    <button type="button" class="btn btn-link">Link</button>
+  `);
