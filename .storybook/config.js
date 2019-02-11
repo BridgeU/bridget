db2425786698f9488598d9419598ccd693eef979
Gonçalo Morais <goncalo@bridge-u.com>
2019-02-11 18:06:42 +0000

    Load stories automatically
    https://storybook.js.org/basics/writing-stories/#loading-stories-dynamically

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,9 @@
 import { configure } from '@storybook/html';
 
+const req = require.context('../src/stories', true, /\.js$/);
+
 function loadStories() {
-  require('../src/stories/index.js');
-  // You can require as many stories as you need.
+  req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
