c40b333ffeacd2da0dc21b9e3f1d0369e8e1bd82
Gonçalo Morais <goncalo@bridge-u.com>
2019-02-11 18:07:03 +0000

    Start the first proper story, Content
    Start by replicating a slimmer version of Bootstrap’s documentation,
    https://getbootstrap.com/docs/4.2/content/typography

--- src/stories/index.js
+++ src/stories/index.js
@@ -11,15 +11,4 @@ storiesOf('Demo', module)
     button.className = 'btn btn-primary';
     button.addEventListener('click', e => console.log(e));
     return button;
-  })
-  .add('buttons', () => `
-    <button type="button" class="btn btn-primary">Primary</button>
-    <button type="button" class="btn btn-secondary">Secondary</button>
-    <button type="button" class="btn btn-success">Success</button>
-    <button type="button" class="btn btn-danger">Danger</button>
-    <button type="button" class="btn btn-warning">Warning</button>
-    <button type="button" class="btn btn-info">Info</button>
-    <button type="button" class="btn btn-light">Light</button>
-    <button type="button" class="btn btn-dark">Dark</button>
-    <button type="button" class="btn btn-link">Link</button>
-  `);
+  });
