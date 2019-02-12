a5cc331d5aaae79d9f1e617abcd3d64d9f1231a7
Gonçalo Morais <goncalo@bridge-u.com>
2019-02-12 11:17:44 +0000

    Deal with both strings and DOM nodes
    This will give us the option to either write plain strings or
    actual DOM nods, with behaviour and all.

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,5 +3,16 @@ import { configure, addDecorator } from '@storybook/html';
 const req = require.context('../src/stories', true, /\.js$/);
 const loadStories = () => req.keys().forEach(filename => req(filename));
 
-addDecorator(story => `<div class="container m-5">${story()}</div>`);
+const storyAsString = (story) => `<div class="container m-5">${story}</div>`;
+const storyAsNode = (story) => {
+  const wrapper = document.createElement('div');
+  wrapper.className = 'container m-5';
+  wrapper.appendChild(story);
+  return wrapper;
+};
+
+addDecorator(story => {
+  const tale = story();
+  return typeof tale === "string" ? storyAsString(tale) : storyAsNode(tale);
+});
 configure(loadStories, module);
