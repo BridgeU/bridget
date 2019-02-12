7f48662120cce3d258a1bd0c690ea9df6601361a
Gonçalo Morais <goncalo@bridge-u.com>
2019-02-12 16:08:40 +0000

    Remove dev mode from Webpack config
    Not needed for now, I believe. We also don’t need minification for
    now, since we’re building this on other apps — hopefully.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const devMode = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   mode: 'development',
@@ -15,16 +14,14 @@ module.exports = {
     libraryTarget: 'umd'
   },
   plugins: [
-    new MiniCssExtractPlugin({
-      filename: devMode ? 'bridget.[hash].css' : 'bridget.css'
-    })
+    new MiniCssExtractPlugin({ filename: 'bridget.css' })
   ],
   module: {
     rules: [
       {
         test: /\.(sa|sc|c)ss$/,
         use: [
-          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          MiniCssExtractPlugin.loader,
           'css-loader',
           'sass-loader',
         ],
