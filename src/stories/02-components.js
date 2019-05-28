827a69de2f614630e1f2fc3ead34b3c425037cef
Gonçalo Morais <goncalo@bridge-u.com>
2019-05-28 17:36:05 +0100

    Add a full form example
    This is not an extensive example of all the possibilities a form
    has, but it is quite representative of the major building blocks
    we’ll be using to build most of our forms.
    
    Alex is still to look into these and to provide an updated look,
    but for now this example will serve as a testing pad for the
    changes he’ll suggest soon.

--- src/stories/02-components.js
+++ src/stories/02-components.js
@@ -46,20 +46,92 @@ storiesOf('Components', module)
     <button type="button" class="btn btn-success btn-fab"><i class="material-icons">add</i></button>
     <button type="button" class="btn btn-danger btn-fab"><i class="material-icons">remove</i></button>
   `)
-  .add('Text input - with label', () => `
-    <div class="form-group">
-      <label for="exampleInputEmail1">Email address</label>
-      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    </div>
-  `)
-  .add('Text input - without label', () => `
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email address">
-  `)
-  .add('Select input', () => `
-    <select class="custom-select">
-      <option selected>Open this select menu</option>
-      <option value="1">One</option>
-      <option value="2">Two</option>
-      <option value="3">Three</option>
-    </select>
+  .add('Forms', () => `
+    <h5>Forms</h5>
+
+    <form>
+      <div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Placeholder text">
+        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+      </div>
+
+      <div class="form-group">
+        <label for="exampleReadOnly">Readonly</label>
+        <input class="form-control" type="text" id="exampleReadOnly" placeholder="Readonly input here..." readonly>
+      </div>
+
+      <div class="form-group">
+        <label for="exampleFormControlSelect1">Example select</label>
+        <select class="form-control" id="exampleFormControlSelect1">
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </select>
+      </div>
+
+      <div class="form-group">
+        <label for="exampleFormControlSelect2">Example multiple select</label>
+        <select multiple class="form-control" id="exampleFormControlSelect2" aria-describedby="multiSelectHelp">
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </select>
+        <small id="multiSelectHelp" class="form-text text-muted">
+          Press Command (for macOS) or Control (for Windows) while clicking the options to select multiple item
+        </small>
+      </div>
+
+      <div class="form-group">
+        <label for="exampleFormControlTextarea1">Example textarea</label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+      </div>
+
+      <div class="form-group">
+        <label for="exampleFormControlFile1">Example file input</label>
+        <input type="file" class="form-control-file" id="exampleFormControlFile1">
+      </div>
+
+      <div class="form-group">
+        <legend class="col-form-label">Checkboxes</legend>
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" id="gridCheck1" checked>
+          <label class="form-check-label" for="gridCheck1">First checkbox</label>
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" id="gridCheck2">
+          <label class="form-check-label" for="gridCheck2">Second checkbox</label>
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" id="gridCheck3" disabled>
+          <label class="form-check-label" for="gridCheck3">Second checkbox</label>
+        </div>
+      </div>
+
+      <fieldset class="form-group">
+        <legend class="col-form-label">Radios</legend>
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked>
+          <label class="form-check-label" for="gridRadios1">
+            First radio
+          </label>
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2">
+          <label class="form-check-label" for="gridRadios2">
+            Second radio
+          </label>
+        </div>
+        <div class="form-check disabled">
+          <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="option3" disabled>
+          <label class="form-check-label" for="gridRadios3">
+            Third disabled radio
+          </label>
+        </div>
+      </fieldset>
+    </form>
   `);
