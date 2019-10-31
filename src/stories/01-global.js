71b6708eec7d6aed5036db2f102b203ee351ad4a
Shrihari Sankaran <shrihari@bridge-u.com>
2019-10-31 13:26:22 +0000

    Sorting out greys
    Adding variables for all the greys we'll need in the product and replacing old grey variables with the new ones.

--- src/stories/01-global.js
+++ src/stories/01-global.js
@@ -5,6 +5,12 @@ import {
   cardinalRed,
   flamingoRed,
   finchBlue,
+  grey1,
+  grey2,
+  grey3,
+  grey4,
+  grey5,
+  grey6,
 } from '../../.storybook/storybook.scss'
 
 storiesOf('Global', module)
@@ -55,4 +61,54 @@ storiesOf('Global', module)
         <pre class="card-text font-weight-bold">$finch-blue</pre>
       </div>
     </div>
+
+    <br />
+    <div style="clear:both;"></div>
+    <br />
+
+    <h3>Greys</h3>
+
+    <div class="card storybook__card float-left mr-3 mb-3">
+      <div class="card-img-top storybook__card-img-top" style="background: ${grey1}"></div>
+      <div class="card-body">
+        <span class="card-text">${grey1}</span>
+        <pre class="card-text font-weight-bold">$grey-100</pre>
+      </div>
+    </div>
+    <div class="card storybook__card float-left mr-3 mb-3">
+      <div class="card-img-top storybook__card-img-top" style="background: ${grey2}"></div>
+      <div class="card-body">
+        <span class="card-text">${grey2}</span>
+        <pre class="card-text font-weight-bold">$grey-200</pre>
+      </div>
+    </div>
+    <div class="card storybook__card float-left mr-3 mb-3">
+      <div class="card-img-top storybook__card-img-top" style="background: ${grey3}"></div>
+      <div class="card-body">
+        <span class="card-text">${grey3}</span>
+        <pre class="card-text font-weight-bold">$grey-300</pre>
+      </div>
+    </div>
+    <div class="card storybook__card float-left mr-3 mb-3">
+      <div class="card-img-top storybook__card-img-top" style="background: ${grey4}"></div>
+      <div class="card-body">
+        <span class="card-text">${grey4}</span>
+        <pre class="card-text font-weight-bold">$grey-400</pre>
+      </div>
+    </div>
+    <div class="card storybook__card float-left mr-3 mb-3">
+      <div class="card-img-top storybook__card-img-top" style="background: ${grey5}"></div>
+      <div class="card-body">
+        <span class="card-text">${grey5}</span>
+        <pre class="card-text font-weight-bold">$grey-500</pre>
+      </div>
+    </div>
+    <div class="card storybook__card float-left mr-3 mb-3">
+      <div class="card-img-top storybook__card-img-top" style="background: ${grey6}"></div>
+      <div class="card-body">
+        <span class="card-text">${grey6}</span>
+        <pre class="card-text font-weight-bold">$grey-600</pre>
+      </div>
+    </div>
+
   `);
